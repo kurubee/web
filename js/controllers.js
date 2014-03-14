a7598da3011d7ac6d4a965ecdb259f94a11b3fbf
Pedro <pedro@pedro-SATELLITE-L735.(none)>
2014-03-14 12:04:06 -0500

    Make the PUT request work when saving course info

--- js/controllers.js
+++ js/controllers.js
@@ -50,15 +50,15 @@ kurubeeApp.controller('CourseDetailCtrl', function($scope, Restangular,$cookieSt
         {name:'French', code:'fr'},
         {name:'Arabic', code:'ar'}
     ];
+    $scope.career_types = ["explore","exam"];
     Restangular.setDefaultHeaders({"Authorization": "ApiKey "+$cookieStore.get("username")+":"+$cookieStore.get("token")});
     var baseCourse = Restangular.one('career', $routeParams.courseId);
     $scope.user = $cookieStore.get("username");
     jsPlumb.ready(function() {
         jsPlumb.Defaults.Container = $("#row");
-        //jsPlumb.Defaults.Endpoints = [ [ "Dot", 7 ], [ "Dot", 7 ] ];
-        			var stateMachineConnector = {				
-				connector:"Straight",
-            paintStyle:{ lineWidth:1.5, strokeStyle:"green" },
+			var stateMachineConnector = {				
+				connector:"Flowchart",
+                paintStyle:{ lineWidth:1.5, strokeStyle:"green" },
 				endpoint:"Blank",
 				anchor:"Continuous",
 				overlays:[ ["PlainArrow", {location:1, width:15, length:12} ]]
@@ -66,26 +66,44 @@ kurubeeApp.controller('CourseDetailCtrl', function($scope, Restangular,$cookieSt
         jsPlumb.connect({
             source:"title-description", 
             target:"title-input"},stateMachineConnector);
-/*            anchors:[ [ 0.52,0.5, 0, 0 ],[ 0.3,0, 0, 0 ] ],
-            paintStyle:{ lineWidth:1, strokeStyle:"#858C91" },
-            connector: "StateMachine",
-            endpoint:"Blank",
-            endpointStyles:[{fillStyle:"#858C91"}, {fillStyle:"#858C91"}]
-        });*/
+
         jsPlumb.connect({
             source:"description-description", 
             target:"description-input"},stateMachineConnector);
-            /*anchors:[ "RightMiddle",[ 0.3,0, 0, 0 ] ],
-            paintStyle:{ lineWidth:1, strokeStyle:"#858C91" },
-            connector:"StateMachine",
-            endpoint:"Blank",
-            endpointStyles:[{fillStyle:"#858C91"}, {fillStyle:"#858C91"}]
-        });*/
     });
     baseCourse.get().then(function(course1){
         $scope.course = Restangular.copy(course1);
+        console.log($scope.course);
+        for (var i in $scope.languages)
+        {
+            if($scope.languages[i].code == $scope.course.language_code)
+            {
+                $scope.language = $scope.languages[i];
+                console.log($scope.language);
+            }
+        }
+        for (var j in $scope.career_types)
+        {
+            if($scope.career_types[j] == $scope.course.career_type)
+            {
+                $scope.career_type = $scope.career_types[j];
+                console.log($scope.career_type);
+            }
+        }
     });
+
     $scope.save = function() {
+        for (var i in $scope.languages)
+        {
+            console.log($scope.language);
+            if($scope.languages[i].name == $scope.language.name)
+            {
+                $scope.course.language_code = $scope.languages[i].code;
+                console.log($scope.course.language_code);
+            }
+        }
+        console.log($scope.career_type);
+        $scope.course.career_type = $scope.career_type;
         $scope.course.put();
     };
 });
