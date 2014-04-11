2d0ee6617a3cff944543854de3df625897d84b1d
Pedro <pedro@pedro-SATELLITE-L735.(none)>
2014-04-11 17:47:32 -0500

    Make the background image fixed in the activity list

--- js/controllers.js
+++ js/controllers.js
@@ -98,7 +98,7 @@ kurubeeApp.controller('CourseDetailCtrl',['Aux', '$scope', '$location','Restangu
 }]);
 
 
-kurubeeApp.controller('LevelDetailCtrl', ['Aux', '$scope', '$location', 'Restangular','$cookieStore', '$routeParams', function(Aux, $scope, $location, Restangular,$cookieStore, $routeParams) {
+kurubeeApp.controller('LevelDetailCtrl', ['Aux', '$route', '$scope', '$location', 'Restangular','$cookieStore', '$routeParams', function(Aux, $route, $scope, $location, Restangular,$cookieStore, $routeParams) {
     Restangular.setDefaultHeaders({"Authorization": "ApiKey "+$cookieStore.get("username")+":"+$cookieStore.get("token")});
     var baseActivities = Restangular.all('editor/activity/?level_type=' + $routeParams.levelId + '&career=' + $routeParams.courseId);
     $scope.level = $routeParams.levelId;
@@ -134,7 +134,7 @@ kurubeeApp.controller('LevelDetailCtrl', ['Aux', '$scope', '$location', 'Restang
        var baseActivity = Restangular.one('editor/activity', activity.id);
        baseActivity.remove().then(function(){
            $scope.loaded = true;
-           console.log('listo!');
+           $route.reload();
        });    
     };
 }]);
