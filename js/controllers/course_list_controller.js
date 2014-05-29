7fde628816b0182b128d3b7cc9b45fbbd3300e39
Pedro <pedro@pedro-SATELLITE-L735.(none)>
2014-05-28 22:56:40 -0500

    Filter the careers querysery to only show the careers that he or she owns

--- js/controllers/course_list_controller.js
+++ js/controllers/course_list_controller.js
@@ -1,9 +1,11 @@
  
 kurubeeApp.controller('CourseListCtrl', function($location, $scope, Restangular,$cookieStore) {
+    $scope.loaded=false;
     Restangular.setDefaultHeaders({"Authorization": "ApiKey "+$cookieStore.get("username")+":"+$cookieStore.get("token")});
     var baseCourses = Restangular.all('editor/career');
     // This will query /courses and return a promise.
     baseCourses.getList().then(function(courses) {
+        $scope.loaded=true;
         $scope.courses = courses
     });
     $scope.orderProp = 'timestamp';
